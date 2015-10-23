28a58c8d0cc82fc15f9a66b64fc8b3c9d9b07736
mocchi <boom.boom.planet@gmail.com>
2015-10-23 23:26:23 +0900

    add test

--- src/test/java/io/mocchit/api/Channels.java
+++ src/test/java/io/mocchit/api/Channels.java
@@ -5,7 +5,7 @@ import io.mocchit.jslack.SlackClient;
 public class Channels {
 
 	public static void main(String[] args) {
-		String token = "xoxp-2664788996-2685722346-3032510289-34e43c";
+		String token = "xoxp-xxxxxxxxxxxxxxxxxxxxxxxx";
 		SlackClient slackClient = new SlackClient(token);
 		slackClient.list();
 	}
