f40bb3e5009401972eb6f6bc37e3563db172e006
mocchi <boom.boom.planet@gmail.com>
2015-10-24 13:23:23 +0900

    fix shorten

--- src/main/java/io/mocchit/jslack/api/method/Auth.java
+++ src/main/java/io/mocchit/jslack/api/method/Auth.java
@@ -4,9 +4,9 @@ import io.mocchit.jslack.api.BaseAPI;
 import io.mocchit.jslack.api.Result;
 
 public interface Auth extends BaseAPI {
-	default Result testAuth(){
-		System.out.println(getToken());
-		return send("auth.test", "token=" + getToken());
+	
+	default Result testAuth() {
+		return send("auth.test", getTokenParam());
 	}
 
 }
